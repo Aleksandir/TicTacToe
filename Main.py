dfbc3c6c3a80e85315c6c3880461fcda0b258440
Aleks B <aleksandir@me.com>
2023-10-23 18:00:42 +1030

    Refactor: changed from %s to F strings

--- Main.py
+++ Main.py
@@ -10,11 +10,11 @@ class Board:
         self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
     def display(self):
-        print(" %s | %s | %s " % (self.cells[1], self.cells[2], self.cells[3]))
+        print(f" {self.cells[1]} | {self.cells[2]} | {self.cells[3]} ")
         print("-----------")
-        print(" %s | %s | %s " % (self.cells[4], self.cells[5], self.cells[6]))
+        print(f" {self.cells[4]} | {self.cells[5]} | {self.cells[6]} ")
         print("-----------")
-        print(" %s | %s | %s " % (self.cells[7], self.cells[8], self.cells[9]))
+        print(f" {self.cells[7]} | {self.cells[8]} | {self.cells[9]} ")
 
 
 board = Board()
