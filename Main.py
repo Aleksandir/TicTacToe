55af480fa84ff2fd9564132674d1d4b83691351c
Aleks B <aleksandir@me.com>
2023-10-23 17:52:30 +1030

    Feat: add ability to print tictactoe board

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
 # tic tac toe game using classes
+import os
+
+os.system("clear")
+
+
+class Board:
+    def __init__(self):
+        # init spaces for board
+        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    def display(self):
+        print(" %s | %s | %s " % (self.cells[1], self.cells[2], self.cells[3]))
+        print("-----------")
+        print(" %s | %s | %s " % (self.cells[4], self.cells[5], self.cells[6]))
+        print("-----------")
+        print(" %s | %s | %s " % (self.cells[7], self.cells[8], self.cells[9]))
+
+
+board = Board()
+board.display()
