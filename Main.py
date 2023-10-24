17d7550ecf0fa450700764747b8d67a4b01fac23
Aleks B <aleksandir@me.com>
2023-10-24 16:58:13 +1030

    Add: header printing and user input
    1. x & o can now enter input
    2. header is now displayed

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # tic tac toe game using classes
 import os
+from re import T
 
 os.system("clear")
 
@@ -16,6 +17,35 @@ class Board:
         print("-----------")
         print(f" {self.cells[7]} | {self.cells[8]} | {self.cells[9]} ")
 
+    def update_cell(self, cell_num, player):
+        self.cells[cell_num] = player
+
 
 board = Board()
-board.display()
+
+
+def print_header():
+    print("Welcome to Tic Tac Toe!\n")
+
+
+def refresh_screen():
+    # clear the screen
+    os.system("clear")
+    # print the header
+    print_header()
+    # show the board
+    board.display()
+
+
+while True:
+    refresh_screen()
+
+    # get X input
+    x_choice = int(input("\nX) Please choose 1-9. > "))
+
+    # update board
+    board.update_cell(x_choice, "X")
+
+    o_choice = int(input("\nO) Please choose 1-9. > "))
+
+    board.update_cell(o_choice, "O")
